dcb23fba9d390be93486334bd615c25d0fc6a20d
BillyLx <1920999764@qq.com>
2022-11-25 18:26:37 +0800

    新增：1.spibit测试代码。

--- BZ20-RRU/app/bz20test/testcase.c
+++ BZ20-RRU/app/bz20test/testcase.c
@@ -11,11 +11,13 @@ void FT_mainTest(uint32_t ulMode,uint32_t ulLogLevel)
     printf("\n|***************************************************\n");
     printf("----------------%s------------------\n",str[ulMode-1]);
     printf("|***************************************************\n");
-    //RRUrun(mode);
+    RRUrun(ulMode);
     
     //1.测试SPI读写 
     FT_SpiTest(0x1CE);
     
+    //测试SPI,PIN bit读写
+    FT_SpiBitTest(0x1CE);
     //close_device();
     gulLogLevel = gulLogLevel_bak;
 }
@@ -50,5 +52,40 @@ void FT_SpiTest(uint32_t Addr)
     }
     else {
         BZ_LOGPRINT("TestCase:%d ErrCNT:%d Ok",ulTestCase,ulRET);
+    }
+}
+
+void FT_SpiBitTest(uint32_t Addr)
+{
+    uint32_t i=0, ulBitEnd=0, ulBitStart=0, ulBitdata = 0, ulRET=0, ulTestCase = 10000, ulBitdataRead=0;
+        // 1.for循环，多少次
+    for ( i = 0; i < ulTestCase; i++)
+    {
+        BZ_DBGPRINT("\n第%d批:%s测试",i,__FUNCTION__);
+        //2. 生成ulBitEnd的随机数
+        ulBitEnd = FT_Rand(sizeof(uint32_t)*8-1,0);
+        //3. 生成ulBitStart的随机数
+        if(ulBitEnd>0)ulBitStart = FT_Rand(ulBitEnd,0);
+        else ulBitStart = 0;
+        //4. 生成ulBitdata
+        ulBitdata = FT_Rand(pow(2,(ulBitEnd-ulBitStart+1))-1,0);
+        BZ_DBGPRINT("用例：<%d:%d>:%d",ulBitEnd,ulBitStart,ulBitdata);     
+        //5.写ulBitdata
+        write_regbit(Addr, ulBitEnd, ulBitStart,ulBitdata);
+        //6.读ulBitdata
+        ulBitdataRead = read_regbit(Addr, ulBitEnd,ulBitStart);
+        if(ulBitdata != ulBitdataRead)
+        {
+            BZ_ERRPRINT("write_regBit:%d,read_regBit:%d",ulBitdata,ulBitdataRead);
+            ulRET++;
+        }
+        usleep(100000);
+    }
+    if(ulRET > 0)
+    {
+        BZ_LOGPRINT("BitTestCase:%d ErrCNT:%d Error",ulTestCase,ulRET);
+    }
+    else {
+        BZ_LOGPRINT("BitTestCase:%d ErrCNT:%d Ok",ulTestCase,ulRET);
     }
 }
